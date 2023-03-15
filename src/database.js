48e4f915a46cfc1268b43db5fb22da942fc312ed
fenrirRGK <arekusumf@gmail.com>
2023-03-15 14:36:48 -0500

    changed connection string cosmosdb

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:XcKSTIbzgEWTiRkD@cluster0.mr5mii3.mongodb.net/test')
+mongoose.connect('mongodb://amuproject:FeV0pykdMKkdXTjVLQFG0ZhU4HvEohhOut5Y61DkMG6ueTE7mVBfZoFng1it4Alce57lEaOjtrHJACDb2BSfQg==@amuproject.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@amuproject@')
 .then((res)=>{
     console.log("db is conected");
 }).catch((err)=>{
