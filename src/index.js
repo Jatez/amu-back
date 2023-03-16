1583aef684c301d2e780e9ad459f96b68ddb084f
fenrirRGK <arekusumf@gmail.com>
2023-03-15 20:28:50 -0500

    console log serv active

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const {mongoose} = require("./database");
 const cors = require("cors");
 const equipos = require("./routes/equipos");
-const laboratorios = require("./routes/laboratorios");
-const eventos = require("./routes/eventos");
+//const laboratorios = require("./routes/laboratorios");
+//const eventos = require("./routes/eventos");
 const certificados = require("./routes/certificados");
 
 
@@ -14,15 +14,15 @@ app.use(express.json());
 
 //Cada una de las rutas
 app.use("/equipos", equipos);
-app.use("/laboratorios", laboratorios);
-app.use("/eventos", eventos);
+//app.use("/laboratorios", laboratorios);
+//app.use("/eventos", eventos);
 app.use("/certificados", certificados);
 
 app.listen(process.env.PORT || 3000,() => {
     console.log(`Server Started at ${3001}`)
 })
 
-alert.apply('Connect')
+console.log('Serv active')
 
 
 module.exports = app;
